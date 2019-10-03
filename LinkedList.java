9e204d01133acee3c1fdb39f37769acfbe8d8dc4
mason lane <mlane0921@hotmail.com>
2019-10-02 20:08:56 -0600

    Added comments

--- LinkedList.java
+++ LinkedList.java
@@ -1,9 +1,17 @@
+/*ENSE 374 Lab 2 Assignment
+  Mason Lane - 200376573
+  2019-10-02
+  This app will build, and traverse a linked list. Users may retrive or delete data.
+  Doubly linked list is included in second branch
+*/
+
+
 import java.util.Scanner;		/* user input */
 
 
 public class LinkedList {
 
-ListElement head;
+ListElement head;			/*keeping track of our head/tail pointers*/
 ListElement tail;
 private int length;
 private int p;
@@ -12,21 +20,21 @@ private int p;
 
 public class ListElement {
 	private ListElement next;
-	/*private ListElement previous;*/
+	/*private ListElement previous;*//*for our doubly linked later*/
 	private int data;
 
 	public ListElement(){
 		this.data = 0;
-		this.next = null;
-		/*this.previous = null;*/		
+		this.next = null;		/*just our constructor */
+		/*this.previous = null;*/	/*for our later double link */		
 	}
 
-	public void setData(int data){
+	public void setData(int data){		/*sets the value of an element's data */
 		this.data = data;
 	}
 
 	public int getData(){
-		return this.data;
+		return this.data;		/* for displaying data */
 	}
 
    }
@@ -34,15 +42,15 @@ public class ListElement {
 
  public void addElement(int data) 
 	{
-		ListElement temp = new ListElement();
-		temp.setData(data);
+		ListElement temp = new ListElement();	/*temp element */
+		temp.setData(data);			/*set the data */
 
 		if (this.head == null) {
            		head = temp;
-            		head.next = tail;
+            		head.next = tail;		/*if we have no head, this is our head */
 			tail = head;
        		 } else {
-                tail.next = temp;
+                tail.next = temp;			/* else, our value becomes the new tail */
                 tail = temp;
             }
         
@@ -55,17 +63,17 @@ public void getElement(int pos)
 		p=0;
 		while(p != pos && p<length)
 		{
-		temp = temp.next;
+		temp = temp.next;			/* traverse list until we reach the element */
 		p++;
 		}
 
 		if(p>length)
 		{
-		System.out.println("Sorry, but this position is beyond the list size"+ "\n");
+		System.out.println("Sorry, but this position is beyond the list size"+ "\n"); /*error checking if user enters an int too large*/
 		}
 		else if(p == pos)
 		{
-		System.out.println("\n"+"Retrieved value: " + temp.getData()+ "\n");		
+		System.out.println("\n"+"Retrieved value: " + temp.getData()+ "\n");		/*displays data of current element*/
 
 
 		}
@@ -81,29 +89,29 @@ public void deleteElement(int pos)
 		p=0;
 		while(p != pos-1 && p<length && pos != 0)
 		{
-		temp = temp.next;
+		temp = temp.next;				/*should stop one element before the intended */
 		p++;
 		}
 
 		if(pos>=length)
 		{
-		System.out.println("Sorry, but this position is beyond the list size"+ "\n");
+		System.out.println("Sorry, but this position is beyond the list size"+ "\n"); /*error check for too large input */
 		}
-		else if(pos == 0)
+		else if(pos == 0)				/*if the user wants to delete the head*/
 		{
 		head = temp.next;
 		length--;
 		System.out.println("Element Deleted"+ "\n");	
 
 		}
-		else if(p == length-2)
+		else if(p == length-2)				/*if the user wants to delete the tail */
 		{
 			tail = temp;
 			temp.next = null;
 			System.out.println("Element Deleted"+ "\n");	
 		length--;	
 		}
-		else if(p == pos-1)
+		else if(p == pos-1)				/*if the user wants to delete an element (not head/tail) */
 		{
 			
 		temp2 = temp.next;
@@ -130,14 +138,14 @@ public void deleteElement(int pos)
     { 
 	if (length != 0)
 	{
-        ListElement n = head; 
-        while (n != null) { 
-            System.out.print(n.getData()+ " "); 
-            n = n.next; 
+        ListElement temp = head; 
+        while (temp  != null) { 
+            System.out.print(temp .getData()+ " "); 	/*starting from head, we move down the list displaying its contents*/
+           temp  = temp.next; 
         } 
 	}else
 	{
-	 System.out.print("list is empty"); 
+	 System.out.print("list is empty"); 		/*if the user has emptied the list by deletion or other means */
 	}
     } 
 
@@ -145,48 +153,49 @@ public void deleteElement(int pos)
     { 
         ListElement n = head; 
         while (n != null) { 
-            System.out.print(n.getData()+ " "); 
+            System.out.print(n.getData()+ " "); 	plaseholder for printing list from tail-head
             n = n.next; 
         } 
     } 
 */
 
-public static void main(String[] args) 
+public static void main(String[] args) 			/* main fxn */
     { 
         /* Start with the empty list. */
-        LinkedList llist = new LinkedList(); 
-  	Scanner in = new Scanner(System.in);  
-	int insert;
+        LinkedList llist = new LinkedList(); 		/*initiate our linked list */
+  	Scanner in = new Scanner(System.in);  		/* prepare scanner for input */
+	int insert;					/* preparing ints for various input, etc*/
 	int x;
 	int  choice =0;
 	int pos;
 	int quit=0;
 	
-	System.out.println("Length of list: ");
+	System.out.println("Length of list: ");		/*user enters the size of their desired list */
 	choice = in.nextInt();	
 
 
-	while (quit != 1){
+	while (quit != 1){				/*loop until user quits */
 	for (int i = 0; i<choice; i++){
 
 	System.out.println("Add number to stack: ");
 	x = in.nextInt();
 
-	llist.addElement(x); 
+	llist.addElement(x); 				/*adds a new element until weve reached our length*/
 
 	
 
 	
 	}
-  	choice = 0;
+  	choice = 0;					/*sets to 0. Upon each loop, user will not have to re-enter their list data */
 	System.out.println("_________________________________________________________________________"+"\n");
         System.out.println("Displaying Linked List Head-Tail:");
-        llist.printLinkedListHead(); 
+        llist.printLinkedListHead(); 			/*above displays list and contains some __ for easier reading */
+							/*below allows user to retrieve, delete or quit */
 	System.out.println("\n" + "\n" + "Delete value = 3, Retrieve value = 2, Quit? 1=yes, 0=No:");
 	quit = in.nextInt();
 	System.out.println("\n"+"_________________________________________________________________________"+"\n");
 
-	if(quit == 3)
+	if(quit == 3)					/*if deleting, we do the following */
 	{
 	System.out.println("\n"+"please enter the numerical position (head=0) of the member you wish to delete");
 	pos = in.nextInt();
@@ -194,7 +203,7 @@ public static void main(String[] args)
 	llist.deleteElement(pos);
 	
 	}
-	else if(quit == 2)
+	else if(quit == 2)				/*if retrieving, we do the following */
 	{
 	System.out.println("\n"+"please enter the numerical position (head=0) of the member you wish to retrieve");
 	pos = in.nextInt();
@@ -203,7 +212,7 @@ public static void main(String[] args)
 
 	}
 	
-
+	/*app will exit loop and quit if user quits */
 
 	}
     } 
